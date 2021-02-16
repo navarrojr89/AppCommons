b54f11731a3174eaa863df92a1b0d060fc59a0b8
Luis Navarro <navarrojr89@gmail.com>
2021-02-16 18:01:39 +0100

    Includes a commons project to inherit Product class

---
+++ src/test/java/com/lamn/microservices/appcommons/ApplicationCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamn.microservices.appcommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
