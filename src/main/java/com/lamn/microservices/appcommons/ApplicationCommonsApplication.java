1650eec707b93cb13df135aca1d5f7a0c843fc25
Luis Navarro <navarrojr89@gmail.com>
2021-02-18 18:18:01 +0100

    Includes JavaDocs

--- src/main/java/com/lamn/microservices/appcommons/ApplicationCommonsApplication.java
+++ src/main/java/com/lamn/microservices/appcommons/ApplicationCommonsApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * The type Application commons application.
+ */
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ApplicationCommonsApplication {
