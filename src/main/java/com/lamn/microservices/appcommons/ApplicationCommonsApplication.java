d8908cf6ba52ffde46a4cdbbfdd05f7ae84632c8
Luis Navarro <navarrojr89@gmail.com>
2021-02-20 11:16:39 +0100

    Reorders annotations

--- src/main/java/com/lamn/microservices/appcommons/ApplicationCommonsApplication.java
+++ src/main/java/com/lamn/microservices/appcommons/ApplicationCommonsApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * The type Application commons application.
  */
-@SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ApplicationCommonsApplication {
 
 }
